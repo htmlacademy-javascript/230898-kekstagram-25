037d182b4b59e79a61771a8215945736f54a7e64
Maria <marias50@live.com>
2022-02-20 22:58:29 +0100

    Task 2
    Two functions Task 2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 // Source https://schoolsw3.com/js/js_random.php
-const min = 1000;
-const max = 10000;
+let min= 10;
+let max=100;
 if (min >= 0 && max >= 0 && min <= max) {
 function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
   }
 else  {
   console.log("Mistake!");
 }
-let result = getRndInteger(min,max);
+let result = getRndInteger (min, max);
 console.log(result);
 
 //Функция для проверки максимальной длины строки.
 const str = 'Life, the universe and everything.';
-const value = str.length;
-const max_value = 10;
-if (value < max_value) {
+function getLength(str) {
+     return str.getLength;
+   }
+let strlength = getLength(str);
+let max_value = 10;
+if (strlength < max_value) {
 console.log(true);
 }
 else {
-console.log(false);
-}
+console.log(false);}
